6c9f3a366f860616e6dfedd003c6623e4bda2874
kalagb7 <kalagb7@gmail.com>
2026-01-29 16:47:53 -0800

    Create cors.js

---
+++ cors.js
@@ -0,0 +1,12 @@
+[
+  {
+    "origin": [
+      "https://monetizelt-b235d.web.app",
+      "http://localhost:5000",
+      "https://www.monetizelt.com"
+    ],
+    "method": ["GET", "POST", "PUT", "DELETE", "HEAD"],
+    "responseHeader": ["Content-Type", "x-goog-meta-*"],
+    "maxAgeSeconds": 3600
+  }
+]
